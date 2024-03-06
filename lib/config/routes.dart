0e81e37733c57235d262598b338fcb8c09fe7782
Fazyes <yuriygorgoluyk@gmail.com>
2024-03-06 17:42:13 +0100

    add constants and routes

---
+++ lib/config/routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:github_favourites/presentation/screens/favourites_screen.dart';
+import 'package:github_favourites/presentation/screens/home_screen.dart';
+import 'package:github_favourites/presentation/screens/loading_screen.dart';
+
+class AppRoutes {
+  /// This is the default route that reserved for sign in page
+  /// Commented because doesn't need for the test task
+  // static const String signIn = '/';
+  static const String loading = '/';
+  static const String home = '/home';
+  static const String favouritesRepos = '/favourites-repos';
+
+  static Map<String, WidgetBuilder> get routes => {
+        loading: (context) => const LoadingScreen(),
+        home: (context) => const HomeScreen(),
+        favouritesRepos: (context) => const FavouritesScreen(),
+      };
+}
